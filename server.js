8cc1a778c409dc504ac8ec99bef2597314e1294b
Cesar <cesarwamartinez@gmail.com>
2024-10-21 21:23:02 -0500

    fix-version

---
+++ server.js
@@ -0,0 +1,76 @@
+const express = require("express"); // CREACION DE LA API
+const cors = require("cors"); 
+const mysql = require("mysql2");
+
+
+const app = express()
+app.use(cors())
+
+
+const conec = mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"oyasumipunpun",
+    database:"nodomobiliario"
+});
+
+
+conec.connect((err)=>{
+    if(err){
+        throw err;
+    }
+    console.log("Conectado con exito")
+})
+
+app.get("/datos",(req,res)=>{
+    const sql = "SELECT * FROM productos";
+    conec.query(sql,(err,resultado)=>{
+        if(err){throw err};
+        res.json(resultado)
+    })
+})
+
+
+app.listen(3000,()=>{
+    console.log("Servidor corriendo el puerto 3000")
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // INSERT DATA
+// const sql = "insert into productos(Nombre,Categoria,Dimension) values (?,?,?)";
+// const data = ['Mesa Cakaguate','MOBILIARIO','400x400'];
+// conec.execute(sql,data,(error,resultado,fields)=>{
+//     if(error){
+//         throw error
+//     }
+//     console.log(resultado)
+//     console.log(fields)
+// })
+
+
+
+// // QUERY 
+
+// const sqlQuery = "SELECT * FROM productos"
+// conec.query(sqlQuery,(error,data,field)=>{
+//     if (error){throw error};
+//     console.log(data)
+//     console.log(field)
+
+// })
